a49be6c59e6fb4a5b9dd84dac6af7925f9716872
Penut Chen <penut85420@gmail.com>
2016-05-12 22:40:57 +0800

    hw7

---
+++ project/script.js
@@ -0,0 +1,12 @@
+function coding() {
+	var i, s = document.getElementById("code").value;
+	for (i = 0; i < 100; i++) {
+		var a = Math.floor(Math.random() * 100), b = Math.floor(Math.random() * 100); 
+		s = "var a = " + a + ", b = " + b + ";" + s;
+		s += "foo(a, b);";
+		console.log(s);
+		if (eval(s) != (a + b)) break;
+	}
+	if (i != 100) console.log("Fail");
+	else console.log("Win");
+}
